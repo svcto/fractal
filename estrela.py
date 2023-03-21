929f274f9a62e248abe8bcfabcf825e3ea030403
Samuel Victor Alves Antunes <84642798+svcto@users.noreply.github.com>
2023-03-20 21:12:15 -0300

    comentado

--- estrela.py
+++ estrela.py
@@ -1,25 +1,14 @@
-import turtle
-from random import random, randint
+import turtle   # Importando a biblioteca turtle
+from random import random, randint   # Importando as funções random e randint da biblioteca random
 
-# for i in range(4):
-#     turtle.pensize(5)
-#
-#     turtle.forward(250)
-#     turtle.left(90)
-#     # turtle.forward(250)
-#     # turtle.left(90)
-#     # turtle.forward(250)
-#     # turtle.left(90)
-#     # turtle.forward(250)
-#     # turtle.left(90)
-turtle.pensize(5)
-turtle.colormode(255)
+turtle.pensize(5)   # Configurando o tamanho da caneta
+turtle.colormode(255)   # Configurando o modo de cores da tartaruga para 255 (RGB)
 
+# Criando um loop que vai de 0 a 4999
 for i in range(5000):
-    turtle.color(randint(0, 150),
-          randint(0, 150),
-          randint(0, 150))
-    turtle.forward(200)
-    turtle.left(216)
+    # Configurando uma cor aleatória para a tartaruga usando a função randint do módulo random
+    turtle.color(randint(0, 150), randint(0, 150), randint(0, 150))
+    turtle.forward(200)   # Movendo a tartaruga para frente em 200 unidades
+    turtle.left(216)   # Rotacionando a tartaruga 216 graus para a esquerda
 
-turtle.Screen().exitonclick()
+turtle.Screen().exitonclick()   # Esperando um clique do mouse para fechar a janela gráfica
