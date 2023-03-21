f79fd7f015114a7a4b20ccbe847cdf4675703585
Samuel Victor Alves Antunes <84642798+svcto@users.noreply.github.com>
2023-03-20 21:08:20 -0300

    comentarios

--- arvore.py
+++ arvore.py
@@ -1,30 +1,30 @@
-import turtle
+import turtle   # Importando a biblioteca turtle
 
-turtle.bgcolor("black")
+turtle.bgcolor("black")  # Definindo a cor de fundo da tela como preto
 
-arv = turtle.Turtle()
-arv.pensize(2)
-arv.color("green")
-arv.left(89)
-arv.backward(70)
-arv.speed(199999)
+arv = turtle.Turtle()   # Criando uma instância do objeto turtle
+arv.pensize(2)   # Configurando o tamanho da caneta
+arv.color("green")  # Configurando a cor da caneta
+arv.left(89)   # Rotacionando a tartaruga 89 graus para a esquerda
+arv.backward(70)   # Movendo a tartaruga para trás em 70 unidades
+arv.speed(199999)   # Configurando a velocidade de desenho da tartaruga
 
-def drawTree(i):
-    if i < 15:
+def drawTree(i):   # Definindo a função drawTree que recebe um parâmetro i
+    if i < 15:   # Se i for menor que 15, a função retorna sem fazer nada
         # print("Inside")
         return
     else:
         # print("Quitside")
-        arv.forward(i)
-        arv.color("magenta")
-        arv.circle(2)
-        arv.color("brown")
-        arv.left(30)
-        drawTree(3*i/4)
-        arv.right(60)
-        drawTree(3*i/4)
-        arv.left(30)
-        arv.backward(i)
+        arv.forward(i)   # Move a tartaruga para frente em i unidades
+        arv.color("magenta")   # Configurando a cor da caneta como magenta
+        arv.circle(2)   # Desenhando um círculo de raio 2
+        arv.color("brown")   # Configurando a cor da caneta como marrom
+        arv.left(30)   # Rotacionando a tartaruga 30 graus para a esquerda
+        drawTree(3*i/4)   # Chamando a função drawTree com um valor reduzido de i
+        arv.right(60)   # Rotacionando a tartaruga 60 graus para a direita
+        drawTree(3*i/4)   # Chamando a função drawTree com um valor reduzido de i
+        arv.left(30)   # Rotacionando a tartaruga 30 graus para a esquerda
+        arv.backward(i)   # Movendo a tartaruga para trás em i unidades
 
-drawTree(100)
-turtle.done()
+drawTree(100)   # Chamando a função drawTree com um valor inicial de i igual a 100
+turtle.done()   # Finalizando a janela gráfica```
